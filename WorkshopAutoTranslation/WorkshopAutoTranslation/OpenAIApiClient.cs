2b0f53fcd2398063522e0c2da00cbc2a737dec8d
Kathleen Shen <kathleenshen@microsoft.com>
2023-09-13 17:31:11 -0700

    autotranslation draft code

---
+++ WorkshopAutoTranslation/WorkshopAutoTranslation/OpenAIApiClient.cs
@@ -0,0 +1,50 @@
+﻿
+using OpenAI.Managers;
+using OpenAI;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using static OpenAI.ObjectModels.SharedModels.IOpenAiModels;
+using static System.Net.Mime.MediaTypeNames;
+
+public class OpenAIApiClient
+{
+    private readonly string _apiKey;
+
+    public OpenAIApiClient(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public async Task<string> SendPrompt(string prompt, string model)
+    {
+        string responseBody = string.Empty;
+        var gpt3 = new OpenAIService(new OpenAiOptions()
+        {
+            ApiKey = this._apiKey
+        });
+        var completionResult = await gpt3.CreateCompletion(new CompletionCreateRequest()
+        {
+            Prompt = prompt,
+            Model = model,
+            Temperature = 0.5F,
+            MaxTokens = 100
+        });
+
+        if (completionResult.Successful)
+        {
+            foreach (var choice in completionResult.Choices)
+            {
+                responseBody += choice.Text + Environment.NewLine;
+            }
+        }
+        else
+        {
+            if (completionResult.Error == null)
+            {
+                throw new Exception("Unknown Error");
+            }
+            Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+        }
+        return responseBody;
+    }
+}
---
+++ WorkshopAutoTranslation/WorkshopAutoTranslation/Translation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using OpenAI.Managers;
+using OpenAI;
+class Translation
+{
+    public string ReadFromFile(string filePath)
+    {
+        // Read from a file
+        string sourceStr = File.ReadAllText(filePath);
+        return sourceStr;
+    }
+
+
+
+    public static async Task Main(string[] args)
+    {
+        string folderPath = "C:\\hackathon2023\\workshops\\content\\english\\python-turtle\\";
+        string apiKey = "API-KEY-AZUREAI"; // Replace with your OpenAI API key
+        Translation t = new Translation();
+
+
+
+        // Get a list of all files in the folder
+        string[] files = Directory.GetFiles(folderPath);
+
+
+
+        var azureClient = new AzureAIApiClient(apiKey);
+
+
+
+        foreach (string filePath in files)
+        {
+            string sourceStr = t.ReadFromFile(filePath);
+
+
+
+            // Translate the content of each file to French
+            var response = await azureClient.SendPrompt(sourceStr, "French");
+
+
+
+            // Handle the translation response here
+            // Create a translated file on the user's desktop
+            string fileName = Path.GetFileName(filePath);
+            string translatedFilePath = Path.Combine("C:\\hackathon2023\\workshops\\content\\francais\\python-turtle\\", fileName + "_translated.md" );
+
+
+            File.WriteAllText(translatedFilePath, response);
+
+
+            Console.WriteLine($"Translation for file: {filePath}");
+            Console.WriteLine(response);
+        }
+    }
+}
